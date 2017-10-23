477363578fc50e66a4286245258d5916bcf4ca26
Moshe Srur <moshiko1988@gmail.com>
2017-10-23 23:04:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function add (x, y){
+function add(x, y){
   return x + y
 }
-function subtracts (x, y){
+function subtract(x, y){
   return x - y
 }
-function add (x, y){
-  return x + y
+function multiply(x, y){
+  return x * y
 }
 function add (x, y){
   return x + y
